d769db17d96d4ccd841d9c287eeb3e39ae9c9912
Axent13 <Axent13@yandex.ru>
2022-03-25 13:04:10 +0700

    Сверстал Button2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Button1 from './components/Button1/Button1';
+import Button2 from './components/Button2/Button2';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header/>
       <Footer/>
       <Button1/>
+      <Button2/>
     </div>
   );
 }
---
+++ src/components/Button2/Button2.js
@@ -0,0 +1,11 @@
+import './Button2.scss';
+
+function Button2() {
+  return (
+    <button className="button2" type="button">
+      <div className="button2__text">Button 2</div>
+    </button>
+  );
+}
+
+export default Button2;
