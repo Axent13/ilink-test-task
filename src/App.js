2fcdf388ab5b7e60258d1859ae6c3d35f6dc2083
Axent13 <axent13@yandex.ru>
2022-03-26 11:34:52 +0700

    Свестал FeedbackCard

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import Footer from './components/Footer/Footer';
 import Button1 from './components/Button1/Button1';
 import Button2 from './components/Button2/Button2';
 import Button2Icon from './components/Button2Icon/Button2Icon';
+import FeedbackCard from './components/FeedbackCard/FeedbackCard';
 
 function App() {
   return (
     <div className="App">
+      <FeedbackCard/>
       <Header/>
       <Footer/>
       <Button1/>
---
+++ src/components/FeedbackCard/FeedbackCard.js
@@ -0,0 +1,26 @@
+import './FeedbackCard.scss';
+import '../../assets/no-avatar.png';
+
+function FeedbackCard() {
+  return (
+    <div className="feedback-card">
+      <div className="feedback-card__meta-info">
+        <div className="feedback-card__user-avatar"></div>
+        <div className="feedback-card__user-name-and-position">
+          <p className="feedback-card__user-name">Имя Фамилия</p>
+          <p className="feedback-card__user-position">Должность (обязательно)</p>
+        </div>
+        <p className="feedback-card__feedback-date">ДД.ММ.ГГГГ</p>
+      </div>
+      <p className="feedback-card__feedback">
+        Отличный коллектив, руководители понимают сам процесс работы каждого сотрудника и помогают 
+        всем без исключения. Система KPI позволяет реально хорошо зарабатывать по простому принципу 
+        - чем больше и лучше ты работаешь, тем больше денег получаешь. Соцпакет - отличная страховка
+         ДМС, организовали курсы английского языка бесплатно, оплачивают тренажерный зал. Зарплату 
+         выплачивают всегда вовремя.
+      </p>
+    </div>
+  );
+}
+
+export default FeedbackCard;
